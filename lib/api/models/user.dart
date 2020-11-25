01a4e5995fba0a0e33ecf464815aed4622a42f0f
Krunal <krunalmiracle@gmail.com>
2020-11-25 18:26:32 +0100

    Repaired

--- lib/api/models/user.dart
+++ lib/api/models/user.dart
@@ -2,22 +2,19 @@ class User {
   String id;
   String email;
   String password;
-  String token;
 
-  User({this.id, this.email, this.password, this.token});
+  User({this.id, this.email, this.password});
   factory User.fromJson(Map<String, dynamic> responseData) {
     return User(
         id: responseData['_id'],
         email: responseData['email'],
-        password: responseData['password'],
-        token: responseData['token']);
+        password: responseData['password']);
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['_id'] = this.id;
     data['email'] = this.email;
     data['password'] = this.password;
-    data['token'] = this.token;
     return data;
   }
 }
