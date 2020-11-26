88c5b9505756de2956c0a9d8c63b6badf64f7587
Krunal <krunalmiracle@gmail.com>
2020-11-26 09:43:31 +0100

    Update user.dart
    Updates for Feature JWT release

--- lib/api/models/user.dart
+++ lib/api/models/user.dart
@@ -2,19 +2,21 @@ class User {
   String id;
   String email;
   String password;
-
-  User({this.id, this.email, this.password});
+  String token;
+  User({this.id, this.email, this.password, this.token});
   factory User.fromJson(Map<String, dynamic> responseData) {
     return User(
         id: responseData['_id'],
         email: responseData['email'],
-        password: responseData['password']);
+        password: responseData['password'],
+        token: responseData['token']);
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['_id'] = this.id;
     data['email'] = this.email;
     data['password'] = this.password;
+    data['token'] = this.token;
     return data;
   }
 }
