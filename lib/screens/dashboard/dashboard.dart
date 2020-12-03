8ee5a459c519dabb0ec501fcb72ee46fdee2bca7
Krunal <krunalmiracle@gmail.com>
2020-12-03 02:21:22 +0100

    HomeScreen Added!

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:meet_your_mates/api/services/student_service.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
@@ -13,27 +14,58 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Provides Access to Student and ability to update student in the whole app
-    StudentProvider _studentProvider = Provider.of<StudentProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("DASHBOARD PAGE"),
-        elevation: 0.1,
+      appBar: AppBar(title: Text("Meet Your Mates")),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(
+              color: Colors.blueGrey,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Center(child: Text("User Entered!")),
-          SizedBox(height: 100),
-          RaisedButton(
-            onPressed: () {},
-            child: Text("Logout"),
-            color: Colors.lightBlueAccent,
-          )
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(title: Text('Home'), icon: Icon(Icons.home)),
+          BottomNavyBarItem(title: Text('Search'), icon: Icon(Icons.search)),
+          BottomNavyBarItem(title: Text('Chat'), icon: Icon(Icons.chat_bubble)),
+          BottomNavyBarItem(title: Text('Profile'), icon: Icon(Icons.person)),
         ],
       ),
     );
