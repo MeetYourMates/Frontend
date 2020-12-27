abd3ebc67b953e3c6225e55609f98d99f611c6c9
Krunal <krunalmiracle@gmail.com>
2020-12-27 19:08:14 +0100

    Register UI Complete, Responsive!!

--- lib/screens/Register/register.dart
+++ lib/screens/Register/register.dart
@@ -1,9 +1,8 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 //Services
 import 'package:meet_your_mates/api/services/auth_service.dart';
-//import 'package:meet_your_mates/api/util/shared_preference.dart';
-import 'package:meet_your_mates/api/util/validators.dart';
 //Components
 import 'package:meet_your_mates/components/already_have_an_account_check.dart';
 import 'package:meet_your_mates/components/or_divider.dart';
@@ -13,6 +12,7 @@ import 'package:meet_your_mates/components/text_field_container.dart';
 //Screens
 import 'package:meet_your_mates/screens/Register/background.dart';
 import 'package:provider/provider.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 //Constants
 import '../../constants.dart';
@@ -24,7 +24,30 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final formKey = new GlobalKey<FormState>();
+  //final formKey = new GlobalKey<FormState>();
+  Logger logger = Logger();
 
+  /// [Reactive Form]
+  final form = FormGroup(
+    {
+      'password': FormControl(validators: [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      'passwordConfirmation': FormControl(validators: [Validators.required]),
+      'email': FormControl(validators: [
+        Validators.required,
+        Validators.email,
+      ]),
+      'name': FormControl(validators: [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    },
+    validators: [
+      Validators.mustMatch('password', 'passwordConfirmation'),
+    ],
+  );
   String _username, _password, _confirmPassword;
 
   @override
@@ -33,10 +56,7 @@ class _RegisterState extends State<Register> {
 
     var loading = Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        CircularProgressIndicator(),
-        Text(" Registering ... Please wait")
-      ],
+      children: <Widget>[CircularProgressIndicator(), Text(" Registering ... Please wait")],
     );
     var facebookAuth = () {
       //Not yet implemented
@@ -48,34 +68,24 @@ class _RegisterState extends State<Register> {
       //Not yet implemented
     };
     var doRegister = () {
-      final form = formKey.currentState;
-      if (form.validate()) {
-        form.save();
-        if ((_password == _confirmPassword)) {
-          auth
-              .register(
-            _username,
-            _password,
-          )
-              .then((response) {
-            if (response['status']) {
-              //If status is ok than we make the user login and continue with the process
-              Navigator.pushReplacementNamed(context, '/login');
-            } else {
-              Flushbar(
-                title: "Registration Failed",
-                message: response['message'].toString(),
-                duration: Duration(seconds: 10),
-              ).show(context);
-            }
-          });
-        } else {
-          Flushbar(
-            title: "Invalid Password",
-            message: "Password's Not Same",
-            duration: Duration(seconds: 3),
-          ).show(context);
-        }
+      if (form.valid) {
+        auth
+            .register(
+          _username,
+          _password,
+        )
+            .then((response) {
+          if (response['status']) {
+            //If status is ok than we make the user login and continue with the process
+            Navigator.pushReplacementNamed(context, '/login');
+          } else {
+            Flushbar(
+              title: "Registration Failed",
+              message: response['message'].toString(),
+              duration: Duration(seconds: 10),
+            ).show(context);
+          }
+        });
       } else {
         Flushbar(
           title: "Invalid form",
@@ -84,107 +94,192 @@ class _RegisterState extends State<Register> {
         ).show(context);
       }
     };
-    Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Scaffold(
-        body: Background(
-          child: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "REGISTER",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  SizedBox(height: size.height * 0.03),
-                  Image.asset(
-                    "assets/icons/signup.png",
-                    width: size.width * 0.35,
-                  ),
-                  TextFieldContainer(
-                    child: TextFormField(
-                      autofocus: false,
-                      validator: validateEmail,
-                      onSaved: (value) => _username = value,
-                      cursorColor: kPrimaryColor,
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.person,
-                          color: kPrimaryColor,
-                        ),
-                        hintText: "email",
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  TextFieldContainer(
-                    child: TextFormField(
-                      autofocus: false,
-                      obscureText: true,
-                      validator: (value) =>
-                          value.isEmpty ? "Please enter password" : null,
-                      onSaved: (value) => _password = value,
-                      cursorColor: kPrimaryColor,
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.person,
-                          color: kPrimaryColor,
-                        ),
-                        hintText: "password",
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  TextFieldContainer(
-                    child: TextFormField(
-                      autofocus: false,
-                      obscureText: true,
-                      validator: (value) =>
-                          value.isEmpty ? "Please reenter password" : null,
-                      onSaved: (value) => _confirmPassword = value,
-                      cursorColor: kPrimaryColor,
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.person,
-                          color: kPrimaryColor,
-                        ),
-                        hintText: "confirm password",
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  auth.registeredInStatus == Status.Registering
-                      ? loading
-                      : RoundedButton(text: "REGISTER", press: doRegister),
-                  SizedBox(height: size.height * 0.0),
-                  AlreadyHaveAnAccountCheck(
-                    login: false,
-                    press: () {
-                      Navigator.pushReplacementNamed(context, '/login');
-                    },
-                  ),
-                  //forgotLabel,
-                  OrDivider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    //* RESPONSIVE Register...
+    Size size = MediaQuery.of(context).size * 0.95;
+    return Scaffold(
+      body: SafeArea(
+        child: Background(
+          child: Center(
+            child: SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              child: Container(
+                alignment: Alignment.center,
+                constraints: BoxConstraints.tightForFinite(width: 400, height: size.height * 1),
+                child: ReactiveForm(
+                  formGroup: this.form,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      SocalIcon(
-                        iconSrc: "assets/icons/facebook.png",
-                        press: facebookAuth,
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: Text(
+                            "Register",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24, color: kPrimaryColor),
+                          ),
+                        ),
                       ),
-                      SocalIcon(
-                        iconSrc: "assets/icons/twitter.png",
-                        press: twitterAuth,
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.27,
+                        child: Image.asset(
+                          "assets/icons/signup.png",
+                          //width: size.width * 0.35,
+                        ),
                       ),
-                      SocalIcon(
-                        iconSrc: "assets/icons/google-plus.png",
-                        press: googleAuth,
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: TextFieldContainer(
+                          child: ReactiveTextField(
+                            formControlName: 'name',
+                            autofocus: false,
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.face_rounded,
+                                color: kPrimaryColor,
+                              ),
+                              hintText: "name",
+                              border: InputBorder.none,
+                            ),
+                            validationMessages: (control) =>
+                                {'required': 'The name must not be empty'},
+                          ),
+                        ),
                       ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: TextFieldContainer(
+                          child: ReactiveTextField(
+                            formControlName: 'email',
+                            autofocus: false,
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.email_rounded,
+                                color: kPrimaryColor,
+                              ),
+                              hintText: "email",
+                              border: InputBorder.none,
+                            ),
+                            validationMessages: (control) => {
+                              'required': 'The email must not be empty',
+                              'email': 'The email value must be a valid email'
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: TextFieldContainer(
+                          child: ReactiveTextField(
+                            formControlName: 'password',
+                            obscureText: true,
+                            autofocus: false,
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.code_sharp,
+                                color: kPrimaryColor,
+                              ),
+                              hintText: "password",
+                              border: InputBorder.none,
+                            ),
+                            validationMessages: (control) => {
+                              'required': 'The password must not be empty',
+                              'minLenght': 'The password must have at least 3 characters'
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: TextFieldContainer(
+                          child: ReactiveTextField(
+                            formControlName: 'passwordConfirmation',
+                            autofocus: false,
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.code_sharp,
+                                color: kPrimaryColor,
+                              ),
+                              hintText: "Retype Password",
+                              border: InputBorder.none,
+                            ),
+                            validationMessages: (control) =>
+                                {'required': 'The password confirm field must not be empty'},
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.08,
+                        child: auth.registeredInStatus == Status.Registering
+                            ? loading
+                            : RoundedButton(text: "REGISTER", press: doRegister),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.01,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: SizedBox(height: 6, width: size.width),
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.03,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: AlreadyHaveAnAccountCheck(
+                            login: false,
+                            press: () {
+                              Navigator.pushReplacementNamed(context, '/login');
+                            },
+                          ),
+                        ),
+                      ),
+                      //forgotLabel,
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.01,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: OrDivider(),
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height * 0.1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SocalIcon(
+                              iconSrc: "assets/icons/facebook.png",
+                              press: facebookAuth,
+                            ),
+                            SocalIcon(
+                              iconSrc: "assets/icons/twitter.png",
+                              press: twitterAuth,
+                            ),
+                            SocalIcon(
+                              iconSrc: "assets/icons/google-plus.png",
+                              press: googleAuth,
+                            ),
+                          ],
+                        ),
+                      )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
           ),
