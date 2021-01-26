ddc705d5022f281acb7539705f62f2654b6cb89f
Jose-Verdu-Diaz <pepv@hotmail.es>
2021-01-26 13:11:16 +0100

    Petición implementada, falta actualizar los cambios el el state

--- lib/api/util/app_url.dart
+++ lib/api/util/app_url.dart
@@ -20,8 +20,10 @@ class AppUrl {
   //Students
   static const String addsubjects = baseURL + "/course/addStudent";
   static const String getCourseStudents = baseURL + "/course/getStudents";
-  static const String getStudentsAndCourses = baseURL + "/student/getStudentsAndCourses";
-  static const String getProfessorCoursesAndStudents = baseURL + "/professor/getStudentsAndCourses";
+  static const String getStudentsAndCourses =
+      baseURL + "/student/getStudentsAndCourses";
+  static const String getProfessorCoursesAndStudents =
+      baseURL + "/professor/getStudentsAndCourses";
   static const String editProfileStudent = baseURL + "/student/updateStudent";
   static const String editProfileProfessor = baseURL + "/professor/update";
   static const String editProfilePhoto = baseURL + "/student/StudentPhoto";
@@ -34,8 +36,11 @@ class AppUrl {
   //TeamsProfessor
   static const String getTeams = baseURL + "/team/";
   static const String addTeam = baseURL + "/team/add";
+  static const String updateTeam = baseURL + "/team/update";
   //Projects
-  static const String getCourseProjectsProfessor = baseURL + "/professor/getCourseProjects";
-  static const String getCourseProjectsStudent = baseURL + "/student/getCourseProjects";
+  static const String getCourseProjectsProfessor =
+      baseURL + "/professor/getCourseProjects";
+  static const String getCourseProjectsStudent =
+      baseURL + "/student/getCourseProjects";
   static const String addProject = baseURL + "/project/add";
 }
